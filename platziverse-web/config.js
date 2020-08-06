6a559c07d33a8ba3e819631d1a74b6bb5593ff9f
Alejandro2134 <zapatamolinaalejandro@gmail.com>
2020-08-06 18:15:10 -0500

    Class 57

---
+++ platziverse-web/config.js
@@ -0,0 +1,7 @@
+'use strict'
+
+module.exports = {
+    endpoint: process.env.API_ENDPOINT || 'http://localhost:3000',
+    apiToken: process.env.API_TOKEN || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBsYXR6aSIsImFkbWluIjp0cnVlLCJwZXJtaXNzaW9ucyI6WyJtZXRyaWNzOnJlYWQiXSwiaWF0IjoxNTk2NzQ3MTIwfQ.Tky-lZVs0QQBCFbndAXh47KJx1NTkJ-9zQtAd7YYP1Q',
+    serverHost: process.env.SERVER_HOST || 'http://localhost:8080'
+}
